4e18d9115cfb41fdf899b0603d68ea5fa2138b31
OCCULT705 <gmashinji@gmail.com>
2024-07-17 10:59:50 +0300

    Updated User Authentication Feature

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('welcome');
+    return redirect('/home');
 });
 
 Auth::routes();
