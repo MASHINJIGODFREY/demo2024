3e2855de671f1c14c898ed1a591aea5fcce47f77
OCCULT705 <gmashinji@gmail.com>
2024-07-17 13:13:47 +0300

    Added Security on Todo Manager Routes

--- app/Http/Controllers/todosController.php
+++ app/Http/Controllers/todosController.php
@@ -7,6 +7,16 @@ use App\Models\todos;
 
 class todosController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(){
         $todos=todos::all();
         $data=compact('todos');
