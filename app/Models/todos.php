696be999878e546ee2c949ec7747a359bc08f9fa
MASHINJIG <gmashinji@yahoo.com>
2024-07-19 13:58:33 +0300

    Commenting Todo Model Properties

--- app/Models/todos.php
+++ app/Models/todos.php
@@ -8,6 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class todos extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table='todos';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
     protected $primary_key='id';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
 }
