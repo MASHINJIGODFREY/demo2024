37ddded9cf755ac51b56cc39844d86198f74bc08
MASHINJIGODFREY <gmashinji@gmail.com>
2024-07-19 12:05:34 +0300

    Docummenting Todo Model Properties

--- app/Models/todos.php
+++ app/Models/todos.php
@@ -8,6 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class todos extends Model
 {
     use HasFactory;
+
+    /**
+     * The table name associated with the model.
+     *
+     * @var string
+     */
     protected $table='todos';
+
+    /**
+     * The table column set as primary key.
+     *
+     * @var string
+     */
     protected $primary_key='id';
 }
